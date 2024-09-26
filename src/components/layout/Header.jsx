8a2772546f47a0f5d161653bd570b829ce0e3677
obed-tc <castroobed53@gmail.com>
2024-09-26 00:48:42 -0400

    fix: Fixed header mode dark

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,10 @@ import SwitchComponent from "../common/SwitchComponent";
 function Header() {
   return (
     <header className=" w-full flex justify-between dark:text-white px-5 py-3">
-      <h1>QuickJSON</h1>
+      <h1 className="text-4xl font-bold ">
+        Quick
+        <span className="text-lime-500">JSON</span>
+      </h1>
       <SwitchComponent />
     </header>
   );
