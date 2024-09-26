bcb9199cd46de1105e217c3a17145939f58791d7
obed-tc <castroobed53@gmail.com>
2024-09-26 00:48:05 -0400

    feat: Add footer component

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,17 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="dark:bg-black flex justify-center py-5">
+      <label className="dark:text-white text-xs">
+        Desarrollado por{" "}
+        <a
+          className="text-lime-600 font-bold dark:text-lime-400"
+          href="https://github/obed-tc "
+        >
+          @obed-tc
+        </a>
+      </label>
+    </div>
+  );
 }
 
 export default Footer;
